4432714fc3eb8c6f2931e8ef8d89f3f08f47394c
iavengetheweak <jacob.creech@eagles.oc.edu>
2021-03-22 16:00:42 -0500

    Added first reward with custom firework & messages

--- src/main/java/us/newadventures/rewardingachievements/RewardingAchievements.java
+++ src/main/java/us/newadventures/rewardingachievements/RewardingAchievements.java
@@ -6,12 +6,14 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.mineacademy.fo.Common;
 import org.mineacademy.fo.plugin.SimplePlugin;
 import us.newadventures.rewardingachievements.commands.ExchangeCommand;
+import us.newadventures.rewardingachievements.events.FireworkListener;
 
 public final class RewardingAchievements extends SimplePlugin {
 
 	@Override
 	protected void onPluginStart() {
 
+		registerEvents(new FireworkListener());
 		registerCommand(new ExchangeCommand());
 	}
 
--- src/main/java/us/newadventures/rewardingachievements/commands/ExchangeCommand.java
+++ src/main/java/us/newadventures/rewardingachievements/commands/ExchangeCommand.java
@@ -1,10 +1,14 @@
 package us.newadventures.rewardingachievements.commands;
 
+import org.bukkit.Color;
+import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.entity.Firework;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.mineacademy.fo.command.SimpleCommand;
+import us.newadventures.rewardingachievements.utility.CustomFirework;
 
 public class ExchangeCommand extends SimpleCommand {
 
@@ -15,15 +19,26 @@ public class ExchangeCommand extends SimpleCommand {
 
 	@Override
 	protected void onCommand() {
-
 		checkConsole();
 
+		ItemStack check = new ItemStack(Material.DIRT);
+		ItemStack goods = new ItemStack(Material.DIRT, 32);
+		ItemStack reward = new ItemStack(Material.PHANTOM_MEMBRANE, 1);
+
 		Player player = getPlayer();
+		World world = player.getWorld();
+		CustomFirework firework = new CustomFirework();
+		Location location = player.getLocation();
+		Inventory inventory = player.getInventory();
 
-		player.getInventory().remove(new ItemStack(Material.DIRT, 32));
-
-		player.getWorld().spawn(player.getLocation(), Firework.class);
-
-		tell("A firework has been spawned.");
+		if (inventory.containsAtLeast(check, 32)) {
+			inventory.removeItem(goods);
+			inventory.addItem(reward);
+			firework.spawnFirework(location, 1, Color.LIME);
+			tell("Thank you for the dirt, here is your reward!");
+		} else {
+			firework.spawnFirework(location, 1, Color.RED);
+			tell("Please provide 32 dirt to receive reward");
+		}
 	}
 }
